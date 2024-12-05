e3144db9a235dee27a19bf8ae637b0972064911c
exhsttd <rlisutkina@gmail.com>
2024-12-05 22:15:02 +0300

    svalka svalka

---
+++ SvalkaTvorcheskaya/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using SvalkaTvorcheskaya.ProjectSvalka.Classes;
+
+// Творческая домашка на тему - свалка. Приведены три вида свалок и примеры процесса их очищения
+class Program
+{
+    static void Main()
+    {
+        List<AbstraktnayaSvalka> waste = new List<AbstraktnayaSvalka>()
+        {
+            new PoligonTBO("Полигон ТБО", "Приволжский район"),
+            new BioreaktorPoligon("Биореакторный полигон", "Химическая улица"),
+            new PoligonPromyshlenny("Полигон промышленных отходов", "Советский район")
+        };
+    
+        foreach (AbstraktnayaSvalka dump in waste) 
+        {
+            dump.DisplayInfo();
+            dump.AcceptWaste(10);
+            dump.PrintLocation();
+            Console.WriteLine(); 
+        }
+
+        // Пример очистки мусора на одной из свалок
+        PoligonTBO tboWaste = (PoligonTBO)waste[0]; 
+        tboWaste.DropWaste();
+    }
+}
